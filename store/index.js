981daed64bbc94e0ab2795f7a37785ed67c49f19
takayanomasashi2 <masayatakashi@gmail.com>
2022-01-07 12:09:32 +0900

    cmt

--- store/index.js
+++ store/index.js
@@ -69,7 +69,7 @@ export const actions = {
     try {
       // Create a PaymentIntent with the information about the order
       const result = await axios.post(
-        "https://ecommerce-netlify.netlify.app/.netlify/functions/create-payment-intent",
+        "https://xenodochial-mahavira-0b1fe4.netlify.app/.netlify/functions/create-payment-intent",
         {
           items: getters.cartItems
         },
